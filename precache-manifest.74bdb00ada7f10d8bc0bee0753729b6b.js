ca74c96e0194095e2602d712a771de463caff632
Vasya Peregarkin <slava.kovazin@gmail.com>
2020-01-14 21:38:40 +0500

    Updates

---
+++ precache-manifest.74bdb00ada7f10d8bc0bee0753729b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74f6d926f762d00376b236f47502d8b",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8e9ae2532e58b3c7daca",
+    "url": "/todo-list/static/css/main.023c4fac.chunk.css"
+  },
+  {
+    "revision": "a84075afa5a136b54b87",
+    "url": "/todo-list/static/js/2.ae1fbc59.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-list/static/js/2.ae1fbc59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e9ae2532e58b3c7daca",
+    "url": "/todo-list/static/js/main.0f0cd4b7.chunk.js"
+  },
+  {
+    "revision": "6a75d9a119eb86523888",
+    "url": "/todo-list/static/js/runtime-main.e3baa66b.js"
+  }
+]);
